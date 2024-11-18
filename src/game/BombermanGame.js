56c4f43891584213304c8af66166a9222f40160d
Bhaumik Muliya <bhaumikmuliya1304+github@gmail.com>
2024-11-18 23:55:48 +0530

    :bento: add assets for character, scene, hud and fonts, :heavy_plus_sign: create a scene with stage and hud, with camera offset to make needful scene visible in defined screen size

---
+++ src/game/BombermanGame.js
@@ -0,0 +1,11 @@
+import { Game } from "engine/Game.js";
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from "game/constants/game.js";
+import { BattleScene } from "./scenes/BattleScene.js";
+
+export class BombermanGame extends Game {
+	scene = new BattleScene(this.frameTime, this.camera);
+
+	constructor() {
+		super("body", SCREEN_WIDTH, SCREEN_HEIGHT);
+	}
+}
---
+++ src/game/scenes/BattleScene.js
@@ -0,0 +1,28 @@
+import { Scene } from "engine/Scene.js";
+import { HALF_TILE_SIZE, STAGE_OFFSET_Y } from "game/constants/game.js";
+import { BattleHud } from "game/entities/BattleHud.js";
+import { LevelMap } from "game/entities/LevelMap.js";
+
+export class BattleScene extends Scene {
+	constructor(time, camera) {
+		super();
+
+		this.stage = new LevelMap();
+		this.hud = new BattleHud();
+
+		camera.position = { x: HALF_TILE_SIZE, y: -STAGE_OFFSET_Y };
+	}
+
+	update(time, context, camera) {
+		// Add your main update calls here
+	}
+
+	draw(context, camera) {
+		this.stage.draw(context, camera);
+		this.hud.draw(context);
+	}
+
+	cleanUp() {
+		// Can be used to clean
+	}
+}
